867eb452b4bcbb080a9ab6e3b18660fafd4356e4
Sanja <sanjacvijetic21@gmail.com>
2022-03-29 18:38:10 +0200

    first commit

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(205, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(694, 164);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 182);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(887, 609);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 78);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GALERIJA\r\nPASMINA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.DropDownHeight = 96;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 16;
+            this.comboBox1.Location = new System.Drawing.Point(35, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(911, 802);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pasmine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] cmbfill = { "akita", "beagle", "boxer", "bullterrier", "bulldog", "cattledog", "chihuahua", "chow", "collie", "corgi", "dachshund", "dalmatian", "dane", "doberman", "eskimo", "germanshepherd", "greyhound", "husky", "labrador", "malamute", "malinois", "maltese", "mastiff", "mix", "newfoundland", "ovcharka", "pekinese", "pitbull", "pointer", "pomeranian", "poodle", "pug", "retriever", "rottweiler", "samoyed", "shiba", "shihtzu", "spaniel", "terrier" };
+            for(int i=0; i < cmbfill.Length; i++)
+            {
+                comboBox1.Items.Add(cmbfill[i].ToString());
+            }
+        }
+        
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string link = comboBox1.GetItemText(listBox2.SelectedItem);
+            webBrowser1.Navigate(new Uri(link));
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            string breed = comboBox1.GetItemText(comboBox1.SelectedItem);
+            string apiUrl = String.Format("https://dog.ceo/api/breed/" + breed + "/images");
+            WebClient client = new WebClient();
+            string fResponse = client.DownloadString(apiUrl);
+            dynamic dobj = JsonConvert.DeserializeObject<dynamic>(fResponse);
+            foreach (var i in dobj["message"])
+                listBox2.Items.Add(i.ToString());
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Document.Body.SetAttribute("scroll", "no");
+            var img = webBrowser1.Document.GetElementsByTagName("img")
+                         .Cast<HtmlElement>().FirstOrDefault();
+            var w = img.ClientRectangle.Width;
+            var h = img.ClientRectangle.Height;
+            img.Style = string.Format("{0}: 100%", w > h ? "Width" : "Height");
+        }
+    }
+        
+}
